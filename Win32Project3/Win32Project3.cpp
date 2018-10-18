91721b5674531adab60fce9c3caf2384ca2d330b
zxqaiycx <zxqaiycx@qq.com>
2018-10-18 20:57:01 +0800

    修改wcex.style的值，解决“注册窗口类失败”。

--- Win32Project3/Win32Project3.cpp
+++ Win32Project3/Win32Project3.cpp
@@ -47,23 +47,24 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	//LPCWSTR szWindowClass = TEXT("窗口");		// 窗口类名
 	//LPCWSTR szTitle = TEXT("WindowTitle");	// 窗口标题名
 
-	// 初始化窗口类
+	// 以下初始化窗口类
 	WNDCLASSEX wcex;
 	wcex.cbSize = sizeof(WNDCLASSEX);									// 窗口类的大小
-	wcex.style = WS_BORDER | WS_HSCROLL | WS_VSCROLL | WS_POPUP;		// 窗口类型												// 窗口类型为默认类型
+	wcex.style = CS_HREDRAW | CS_VREDRAW;		// 窗口类型												// 窗口类型为默认类型
 	wcex.lpfnWndProc = WndProc;											// 定义窗口处理函数
 	wcex.cbClsExtra = 0;												// 窗口类无扩展
 	wcex.cbWndExtra = 0;												// 窗口实例无扩展
 	wcex.hInstance = hInstance;											// 当前实例句柄
-	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));	// 窗口图标为默认图标
+	//wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));	// 窗口图标为默认图标
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
 	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);							// 窗口采用箭头光标
 	wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);			// 注意类型转换；
 	wcex.lpszMenuName = NULL;											// 窗口中无菜单
 	wcex.lpszClassName = szWindowClass;									// 窗口类名为前述“窗口示例”
-	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-																		//窗口小图标为默认图标
+	//wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	wcex.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);			//窗口小图标为默认图标
 
-	// 注册窗口类
+	// 以下注册窗口类
 	if (!RegisterClassEx(&wcex))		// 如果注册失败则发出警告
 	{
 		MessageBox(NULL, _T("窗口类注册失败！"), _T("窗口注册"), NULL);
@@ -75,7 +76,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	//	return FALSE;
 	//}
 
-	// 创建窗口
+	// 以下创建窗口
 	hWnd = CreateWindow(
 		szWindowClass,					// 窗口类名
 		szTitle,						// 窗口实例的标题名
@@ -86,16 +87,14 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		NULL,							// 此窗口无主菜单
 		hInstance,						// 创建此窗口的应用程序的当前句柄
 		NULL);							// 不使用该值
+
 	if (!hWnd)							// 创建窗口失败则发出警告
 	{
 		MessageBox(NULL, _T("创建窗口失败！"), _T("创建窗口"), NULL);
 		return 1;
 	}
-
-	// 显示窗口
-	ShowWindow(hWnd, nCmdShow);
-	// 绘制用户区
-	UpdateWindow(hWnd);
+	ShowWindow(hWnd, nCmdShow);			// 显示窗口
+	UpdateWindow(hWnd);					// 绘制用户区
 
 	// 消息循环
 	while (GetMessage(&msg, NULL, 0, 0))// 从消息队列读取消息并放在MSG结构中
@@ -107,6 +106,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	return (int)msg.wParam;				// 消息循环结束即程序终止时将信息返回系统
 }
 
+// 以下定义窗口函数
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
