ac8f8a08931432a3b5f3f172bcfb66fc4953778b
mk01 <mk01@XC030>
2018-11-13 17:38:54 +0800

    create figure toolbar

--- MFCApplication/MiniDraw/resource.h
+++ MFCApplication/MiniDraw/resource.h
@@ -6,17 +6,19 @@
 #define IDP_OLE_INIT_FAILED             100
 #define IDR_MAINFRAME                   128
 #define IDR_MiniDrawTYPE                130
-#define IDR_TOOLBAR1                    313
-#define ID_BUTTONLINE                   32771
-#define ID_BUTTONE                      32775
-#define ID_BUTTONELLIPSE                32775
+#define IDR_TOOLBAR_FIGURE              313
+#define ID_BUTTON_LINE					32779
+#define ID_BUTTON_ELLIPSE               32780
+#define ID_BUTTON_RECT                  32781
+#define ID_BUTTON_POLYGON				32782
+#define ID_BUTTON_FREEHAND				32783
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        315
-#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_RESOURCE_VALUE        322
+#define _APS_NEXT_COMMAND_VALUE         32784
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
