b4259d6e0e808c352e8356252a667a6938718b60
XiaoqianZhang <zxqaiycx@qq.com>
2018-12-03 03:23:47 +0800

    IDW框架

---
+++ ImageProcessing/ImageWarping/IDW.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <math.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+
+#pragma comment(lib, "opencv_world331d.lib")
+
+class IDW
+{
+public:
+    MyClass();
+    ~MyClass();
+
+private:
+
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
---
+++ ImageProcessing/ImageWarping/main.cpp
@@ -0,0 +1,20 @@
+// 
+// Copyright
+// License
+// Author: XiaoqianZhang
+// 
+// 本程序实现了图像变形，使用的方法为反距离加权插值（IDW）
+// Reference:
+// 1.Image Warping with Scattered Data Interpolation
+// 2.Image Warping by Radial Basis Functions: Application to Facial Expressions
+//
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include "IDW.h"
+using namespace cv;
+
+int main()
+{
+    return 0;
+}
