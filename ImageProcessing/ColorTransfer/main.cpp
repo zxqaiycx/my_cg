5d5677398414d471bd1829d8b620536fe7e0506d
XiaoqianZhang <zxqaiycx@qq.com>
2018-12-02 17:22:50 +0800

    将源图像、目标图像、结果图像在同一窗口显示

--- ImageProcessing/ColorTransfer/main.cpp
+++ ImageProcessing/ColorTransfer/main.cpp
@@ -10,25 +10,39 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include "ColorTransfer.h"
-using cv::Mat;
-using cv::imread;
-using cv::namedWindow;
-using cv::imshow;
-using cv::WINDOW_AUTOSIZE;
+using namespace cv;
 
 int main()
 {
+    //Mat source = imread("source0.jpg");
+    //namedWindow("source", WINDOW_AUTOSIZE);
+    //imshow("source", source);
+    //Mat target = imread("target0.jpg");
+    //namedWindow("target", WINDOW_AUTOSIZE);
+    //imshow("target", target);
+    //ColorTransfer example0(source, target);
+    //namedWindow("result", WINDOW_AUTOSIZE);
+    //imshow("result", example0.result_);
+
     Mat source = imread("source0.jpg");
-    namedWindow("source", WINDOW_AUTOSIZE);
-    imshow("source", source);
     Mat target = imread("target0.jpg");
-    namedWindow("target", WINDOW_AUTOSIZE);
-    imshow("target", target);
+    ColorTransfer eg(source, target);
+    Mat matWrite;
+    eg.result_.convertTo(matWrite, CV_8U, 255.0);
+    imwrite("result0.jpg", matWrite);
 
-    ColorTransfer eg0(source, target);
-    namedWindow("result", WINDOW_AUTOSIZE);
-    imshow("result", eg0.result_);
-
-    cv::waitKey(0);
+    // 在同一窗口显示三个图像
+    Mat matAll(Size(eg.source_.cols + eg.target_.cols + 30, eg.source_.rows + eg.result_.rows + 30),
+        eg.source_.type(), Scalar::all(0));
+    Mat matRoi = matAll(Rect(10, 10, eg.source_.cols, eg.source_.rows));
+    eg.source_.copyTo(matRoi);
+    matRoi = matAll(Rect(eg.source_.cols + 20, 10, eg.target_.cols, eg.target_.rows));
+    eg.target_.copyTo(matRoi);
+    matRoi = matAll(Rect(10, eg.source_.rows + 20, eg.result_.cols, eg.result_.rows));
+    eg.result_.copyTo(matRoi);
+    
+    namedWindow("result", WINDOW_NORMAL);
+    imshow("result", matAll);
+    waitKey(0);
     return 0;
 }
