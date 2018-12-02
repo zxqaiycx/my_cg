9ce019bc1487b09f75428824c3f506c60ff0db72
XiaoqianZhang <zxqaiycx@qq.com>
2018-12-03 04:01:33 +0800

    IDW框架

---
+++ ImageProcessing/ImageWarping/IDW.cpp
@@ -0,0 +1,31 @@
+
+#include "IDW.h"
+#include <math.h>
+
+IDW::IDW()
+{
+}
+
+IDW::IDW(const vector<Point>& p_points, const vector<Point>& q_points, const double u = 2.0)
+{
+
+}
+
+IDW::~IDW()
+{
+}
+
+Point IDW::F(const Point& p, const bool simple = true)
+{
+
+}
+
+double IDW::Wi(const int i, const Point& p)
+{
+
+}
+
+double IDW::Fi(const int i, const Point& p)
+{
+
+}
--- ImageProcessing/ImageWarping/IDW.h
+++ ImageProcessing/ImageWarping/IDW.h
@@ -1,25 +1,32 @@
 #pragma once
 
-#include <math.h>
+#include <vector>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
+using std::vector;
+using cv::Point;
 
 #pragma comment(lib, "opencv_world331d.lib")
 
 class IDW
 {
 public:
-    MyClass();
-    ~MyClass();
+    IDW();
+    IDW(const vector<Point>& p_points, const vector<Point>& q_points, const double u = 2.0);
+    ~IDW();
+
+public:
+    // IDW算法中将点p映射到图像变形后点q的函数F，F = ΣWi*Fi，
+    // 对于样本点对，有F(p_i) = q_i
+    // simple为true时，函数Fi中的变换矩阵T当作单位阵
+    Point F(const Point& p, const bool simple = true);
 
 private:
+    double Wi(const int i, const Point& p);
+    double Fi(const int i, const Point& p);
 
+private:
+    vector<Point> p_points_;
+    vector<Point> q_points_;
+    double u_;
 };
-
-MyClass::MyClass()
-{
-}
-
-MyClass::~MyClass()
-{
-}
